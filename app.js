8fd0c97d767646d658919cc3bc657876578b33b4
Ayushman <ayushmanraghav.ra@gmail.com>
2021-06-08 14:33:56 +0530

    bcrypt passwords

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
-require('dotenv').config();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,28 +18,25 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.info('database connected successfully');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.info("database connected successfully");
 });
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
+const User = new mongoose.model("user", userSchema);
 
-const User = new mongoose.model('user', userSchema);
-
-app.get("/", (request   , response) => {
+app.get("/", (request, response) => {
   response.render("home");
 });
 
@@ -50,36 +48,35 @@ app.get("/register", (request, response) => {
   response.render("register");
 });
 
-app.post('/register', (request, response) => {
-    const email = request.body.username;
-    const password = request.body.password;
+app.post("/register", (request, response) => {
+  const email = request.body.username;
+  const password = request.body.password;
 
+  bcrypt.hash(password, saltRounds, (err, hash) => {
     new User({
-        email: email,
-        password: password
-    })
-    .save((err) => {
-        if(!err) response.render('secrets');
-        else response.send('Registration failed');
+      email: email,
+      password: hash,
+    }).save((err) => {
+      if (!err) response.render("secrets");
+      else response.send("Registration failed");
     });
-
+  });
 });
 
-app.post('/login', (request, response) => {
-    const email = request.body.username;
-    const password = request.body.password;
+app.post("/login", (request, response) => {
+  const email = request.body.username;
+  const password = request.body.password;
 
-    User.findOne({email: email}, (err, user) => {
-        if(user){
-                if(user.password === password){
-                    response.render('secrets');
-                }
-                else response.send("password doesn't matches");             
-        }
-        else response.send("login failed");
-    });
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) response.render("secrets");
+        else response.send("password doesn't matches");
+      });
+    } else response.send("login failed");
+  });
 });
 
-app.listen(3000, () => {
+app.listen(3000 || process.env.PORT, () => {
   console.info("server started at port:3000");
 });
